1e0a7c0021ef20f43e71d493ad761520adcaf3ce
Rapha-Y <raph.yuki@gmail.com>
2020-12-15 10:09:02 -0300

    add functionality to form

--- scripts/script.js
+++ scripts/script.js
@@ -3,5 +3,13 @@ function warnUser(event) {
     alert('Sorry, you cannot send a direct e-mail yet.');
 }
 
+function sendEmail(e) {
+    e.preventDefault();
+    let subject = document.getElementById('subject').value;
+    let body = document.getElementById('message').value;
+    console.log(subject, body);
+    window.open(`mailto:raph.yuki@gmail.com?subject=${subject}&body=${body}`);
+}
+
 let btn = document.getElementById('submit-button');
-btn.onclick = warnUser;
+btn.onclick = sendEmail;
